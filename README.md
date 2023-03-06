306c1666021b567cc27e491e089afb1b7f2bbb9c
Danil Kovalev <dannykvlv@gmail.com>
2023-03-06 14:22:15 +0300

    second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PostmanEcho [![Build status](https://ci.appveyor.com/api/projects/status/qv2083e4ylx7u7de?svg=true)](https://ci.appveyor.com/project/danya794/mobilebank)
---
+++ src/test/java/PostmanEchoApiTest.java
@@ -0,0 +1,27 @@
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+//import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+//import static org.apache.commons.codec.digest.UnixCrypt.body;
+
+
+class PostmanEchoApiTestV1 {
+    @Test
+    void shouldReturnAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
